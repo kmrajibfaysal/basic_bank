cd427ef9b727d3b788f1bea90dc8ebfb6f34f82e
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 02:02:35 +0600

    adding basic style of banking page

--- src/script.js
+++ src/script.js
@@ -4,27 +4,24 @@ const passwordInput = document.querySelector('#password-input');
 
 // login validation function
 function validation() {
-  if (
-    emailInput.value === 'kmrajibfaysal@gmail.com' &&
-    passwordInput.value === '#kmrf2206'
-  ) {
-    location.href = './banking.html';
-  } else {
-    alert('Please insert correct information!');
-  }
+    if (emailInput.value === 'kmrajibfaysal@gmail.com' && passwordInput.value === '#kmrf2206') {
+        window.location.href = './banking.html';
+    } else {
+        alert('Please insert correct information!');
+    }
 }
 
 // login button event listener
 loginSubmit.addEventListener('click', validation);
 
 // enter key event listener for password field
-emailInput.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    validation();
-  }
+emailInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        validation();
+    }
 });
-passwordInput.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    validation();
-  }
+passwordInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        validation();
+    }
 });
