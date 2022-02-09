5485d6f0ac778c750699de4e716587a42b9422e6
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-09 23:59:35 +0600

    adding login validation to login page and making a route for perfect data

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ const loginSubmit = document.querySelector('#login-submit');
 const emailInput = document.querySelector('#email-input');
 const passwordInput = document.querySelector('#password-input');
 
+// login validation function
 function validation() {
   if (
     emailInput.value === 'kmrajibfaysal@gmail.com' &&
     passwordInput.value === '#kmrf2206'
   ) {
-    location.href = 'https://www.google.com/';
+    location.href = './banking.html';
   } else {
     alert('Please insert correct information!');
   }
 }
 
+// login button event listener
 loginSubmit.addEventListener('click', validation);
 
+// enter key event listener for password field
+emailInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    validation();
+  }
+});
 passwordInput.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     validation();
