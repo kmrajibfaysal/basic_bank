1893e0f32c2f67a6ac8ab4481c202d83fc0f86eb
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 14:03:18 +0600

    adding functionality to deposit button and added number validation

--- src/bankingScript.js
+++ src/bankingScript.js
@@ -2,7 +2,12 @@ const depositInput = document.querySelector('#deposit-input');
 const depositButton = document.querySelector('#deposit-button');
 const withdrawInput = document.querySelector('#withdraw-input');
 const withdrawButton = document.querySelector('#withdraw-button');
-console.log(depositInput);
-console.log(depositButton);
-console.log(withdrawInput);
-console.log(withdrawButton);
+const depositTotal = document.querySelector('#deposit-total');
+// deposit button event handler
+depositButton.addEventListener('click', () => {
+    const previousDepositAmount = parseInt(depositTotal.innerText);
+    const newDeposit = parseInt(depositInput.value);
+    const newDepositTotal = previousDepositAmount + newDeposit;
+
+    depositTotal.innerText = newDepositTotal;
+});
