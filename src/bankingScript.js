a53153f74d1c451764435041187b730796908637
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 11:20:16 +0600

    fixing issue with tailwind

---
+++ src/bankingScript.js
@@ -0,0 +1,8 @@
+const depositInput = document.querySelector('#deposit-input');
+const depositButton = document.querySelector('#deposit-button');
+const withdrawInput = document.querySelector('#withdraw-input');
+const withdrawButton = document.querySelector('#withdraw-button');
+console.log(depositInput);
+console.log(depositButton);
+console.log(withdrawInput);
+console.log(withdrawButton);
