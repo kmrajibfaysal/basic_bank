7e2b6b98b84be10a64617da52c3d98b536ff7582
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 14:48:00 +0600

    added function to add total balance after deposit

--- src/bankingScript.js
+++ src/bankingScript.js
@@ -3,11 +3,15 @@ const depositButton = document.querySelector('#deposit-button');
 const withdrawInput = document.querySelector('#withdraw-input');
 const withdrawButton = document.querySelector('#withdraw-button');
 const depositTotal = document.querySelector('#deposit-total');
+const balance = document.querySelector('#balance');
 // deposit button event handler
 depositButton.addEventListener('click', () => {
     const previousDepositAmount = parseInt(depositTotal.innerText);
     const newDeposit = parseInt(depositInput.value);
     const newDepositTotal = previousDepositAmount + newDeposit;
-
     depositTotal.innerText = newDepositTotal;
+    depositInput.value = '';
+    const previousBalance = parseInt(balance.innerText);
+    const newBalance = previousBalance + newDeposit;
+    balance.innerText = newBalance;
 });
