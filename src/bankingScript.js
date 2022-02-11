58fb8156c0029ed255f634c2c0384b68c64fb8c5
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-11 17:26:57 +0600

    fixing bug

--- src/bankingScript.js
+++ src/bankingScript.js
@@ -1,13 +1,27 @@
+/* eslint-disable no-param-reassign */
 // deposit button event handler
 const depositBtn = document.querySelector('#deposit-button');
 const totalDeposit = document.querySelector('#deposit-total');
 const depositInput = document.querySelector('#deposit-input');
 const balance = document.querySelector('#balance');
 
+// ******************
+
+function getValue(previousValue, currentValue) {
+    if (parseInt(currentValue.value) !== 'NaN') {
+        const total = parseInt(previousValue.innerText) + parseInt(currentValue.value);
+        previousValue.innerText = total;
+    }
+    return previousValue.innerText;
+}
+// ******************
+
 depositBtn.addEventListener('click', () => {
-    totalDeposit.innerText = parseInt(totalDeposit.innerText) + parseInt(depositInput.value);
-    balance.innerText = parseInt(balance.innerText) + parseInt(depositInput.value);
-    depositInput.value = '';
+    if (depositInput.value > 0) {
+        getValue(totalDeposit, depositInput);
+        balance.innerText = parseInt(balance.innerText) + parseInt(depositInput.value);
+        depositInput.value = '';
+    }
 });
 
 // withdraw button event handler
@@ -16,7 +30,9 @@ const withDrawInput = document.querySelector('#withdraw-input');
 const totalWithdraw = document.querySelector('#withdraw-total');
 
 withdrawBtn.addEventListener('click', () => {
-    totalWithdraw.innerText = parseInt(totalWithdraw.innerText) + parseInt(withDrawInput.value);
-    balance.innerText = parseInt(balance.innerText) - parseInt(withDrawInput.value);
-    withDrawInput.value = '';
+    if (withDrawInput.value > 0) {
+        getValue(totalWithdraw, withDrawInput);
+        balance.innerText = parseInt(balance.innerText) - parseInt(withDrawInput.value);
+        withDrawInput.value = '';
+    }
 });
